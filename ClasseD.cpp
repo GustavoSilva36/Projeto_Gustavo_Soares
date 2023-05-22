a0c72c4c6488ad5b3f2c45a56fd12dbdfd9fb32f
Gustavo Soares <gustavo.silva36@estudante.ufla.br>
2023-05-22 20:42:16 -0300

    Commit Criacao Classe D

---
+++ ClasseD.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+class classeD{
+    private:
+        string D1;
+        int D2;
+    
+    public:
+        void MD1(){
+            cout << "MC1" << endl;
+        }
+
+        void MD2(){
+            cout << "MC2" << endl;
+        }
+};
